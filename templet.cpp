981ecc32cd026c964cf21920ac6e226a735fead5
sanketRmeshram <48240880+sanketRmeshram@users.noreply.github.com>
2019-11-19 18:48:55 +0530

    Create templet.cpp

---
+++ templet.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define ff first
+#define ss second
+#define mp(a,b) make_pair(a,b)
+#define pb(a) push_back(a)
+#define boost ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);cin.exceptions(cin.failbit)
+#define check(x) cout << #x << " : " << (x) << endl
+#define all(v) v.begin(),v.end() 
+#define endl '\n'  
+#define show(a) for(auto i:a) cout<<i<<" ";cout<<endl
+typedef long long ll; 
+typedef unsigned long long ull; 
+typedef long double ld;
+typedef pair< int , int > mypair;
+typedef priority_queue <int> max_heap;
+typedef priority_queue <mypair, vector<mypair>, greater<mypair> > min_heap;
+const int mod=1000000007;
+const int inf=1e9;
+const int N=2e6+10;
+
+int main()
+{
+	boost;
+	
+	return 0;
+}
